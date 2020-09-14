634531409f0d720c627685b19d94dafdc23fd387
n-gras <47317060+n-gras@users.noreply.github.com>
2020-09-14 12:36:59 +0000

    Update regex.py
    Added short comment inside regex.py

--- regex.py
+++ regex.py
@@ -1,5 +1,6 @@
 reg_, test = input().split('|')
 
+# at this point, only accepts 1 character and . as wildcard
 def regex(reg, str) -> bool:
     return reg == str or bool(reg == '.' and str) or not reg
 
