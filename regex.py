1e72fd01d589691f3c09cd9aa1c41712558f4853
n-gras <47317060+n-gras@users.noreply.github.com>
2020-09-16 15:51:49 +0000

    Update regex.py
    Added wildcard comparisons

--- regex.py
+++ regex.py
@@ -5,31 +5,50 @@ sys.setrecursionlimit(10000)
 
 reg_, test = input().split('|')
 
+
 # compare a single character
-def compare_char(reg, str) -> bool:
-    return reg == str or bool(reg == '.' and str) or not reg
+def compare_char(reg, str_) -> bool:
+    return reg == str_ or bool(reg == '.' and str_) or not reg
+
 
 # compare two strings of equal length
-def compare_string(reg, str):
-    if not reg:
+def compare_string(reg, str_) -> bool:
+    if not reg or (reg == '$' and not str_):  # check for end-pattern '$'
         return True
-    elif not str:
+    elif not str_:
         return False
-    elif compare_char(reg[0], str[0]):
-        return compare_string(reg[1:], str[1:])
+    elif len(reg) > 1:
+        # '?' zero or one wildcard
+        if reg[1] == '?':
+                return compare_string(reg[2:], str_) or compare_string(reg[2:], str_[1:])
+
+        # '*' zero or more wildcard
+        elif reg[1] == '*':
+                return compare_string(reg[2:], str_) or compare_string(reg, str_[1:])
+
+        # '+' one ore more wildcard
+        elif reg[1] == '+':
+                return compare_string(reg.replace('+', '*', 1), str_[1:])
+
+    # regular character comparison
+    elif compare_char(reg[0], str_[0]):
+        return compare_string(reg[1:], str_[1:])
     else:
         return False
 
+
 # compare regex with longer strings
-def regex(reg, str):
+def regex(reg, str_) -> bool:
     if not reg:
         return True
-    elif not str:
+    elif not str_:
         return False
-    elif compare_string(reg, str):
+    elif reg[0] == '^':  # check for begin-pattern '^'
+        return compare_string(reg[1:], str_)
+    elif compare_string(reg, str_):
         return True
     else:
-        return regex(reg, str[1:])
+        return regex(reg, str_[1:])
 
 
 print(regex(reg_, test))
